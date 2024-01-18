19d7edb7f1dcbad8da0b9b7a6a3cce5ebadd69a8
mekuanintlegese <mekuanint1212@gmail.com>
2024-01-18 21:06:35 +0300

    monty

--- main.c
+++ main.c
@@ -1,47 +1,47 @@
 #include "monty.h"
 
-vars var;
 
 /**
- * main - Start LIFO, FILO program
- * @ac: Number of arguments
- * @av: Pointer containing arguments
- * Return: 0 Success, 1 Failed
+ * main - Monty bytecode interpreter
+ * @argc: numbers of arguments passed
+ * @argv: arrays of argument
+ * Return: EXIT_SUCCESS on success or EXIT_FAILURE on failure
  */
-int main(int ac, char **av)
+
+int main(int argc, char *argv[])
 {
-	char *opcode;
-
-	if (ac != 2)
+	stack_t *stack_list = NULL;
+	unsigned int Linemunber = 0;
+	FILE *FILE = NULL;
+	char *File_line = NULL, *operator = NULL;
+	size_t len_buffer = 0;
+	/*QUEUE and numbers of nodes from stack*/
+	var.queue = 0;
+	var.stack_len = 0;
+	/* Make sure get two arguments*/
+	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: monty file\n");
-		return (EXIT_FAILURE);
+		error_handler("wrong_argu", Linemunber);
 	}
-
-	if (start_vars(&var) != 0)
-		return (EXIT_FAILURE);
-
-	var.file = fopen(av[1], "r");
-	if (!var.file)
+	/*reading file*/
+	FILE = fopen(argv[1], "r");
+	if (FILE == NULL)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
-		free_all();
-		return (EXIT_FAILURE);
+		error_fopen(argv[1]);
 	}
-
-	while (getline(&var.buff, &var.tmp, var.file) != EOF)
+	/*on_exit functions get active at program termination: Cleaner*/
+	on_exit(free_File_line, &File_line);
+	on_exit(free_stack_list, &stack_list);
+	on_exit(close_File, FILE);
+	while (getline(&File_line, &len_buffer, FILE) != -1)
 	{
-		opcode = strtok(var.buff, " \r\t\n");
-		if (opcode != NULL)
-			if (call_funct(&var, opcode) == EXIT_FAILURE)
-			{
-				free_all();
-				return (EXIT_FAILURE);
-			}
-		var.line_number++;
+		/* Here we process file content and its validations*/
+		Linemunber++;
+		operator = strtok(File_line, "\n\t\r ");
+		if (operator != NULL && operator[0] != '#')
+		{
+			check_if_op_match(operator, &stack_list, Linemunber);
+		}
 	}
-
-	free_all();
-
-	return (EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
